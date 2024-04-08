0e21aa400d8d780219ae885a4e9d967c365c5b2f
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-08 15:25:07 +0200

    Try with space at the end

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,10 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Print the specified text followed by a new line
-        Console.WriteLine("\"Programming is like building a multilingual puzzle");
-    }
-}
+﻿Console.WriteLine("\"Programming is like building a multilingual puzzle ");
