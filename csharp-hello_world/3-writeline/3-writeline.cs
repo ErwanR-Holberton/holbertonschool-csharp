5c7d7e2d596fafbe930433bad43c69a06e9c9b11
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-08 15:19:54 +0200

    Try with a main

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Print the specified text followed by a new line
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+    }
+}
