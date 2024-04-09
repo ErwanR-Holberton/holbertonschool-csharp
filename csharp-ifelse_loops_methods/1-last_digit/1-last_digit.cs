125dd3b1014552588ed7ee4ae546510e37b9e0a2
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-09 16:09:47 +0200

    changed output message for 0

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -11,7 +11,7 @@ class Program
         else if (number % 10 < -5)
             Console.WriteLine("The last digit of " + number + " is " + (-number)%10 + " and is greater than 5");
         else if (number % 10 == 0)
-            Console.WriteLine("The last digit of " + number + " is " + number%10 + " and is 0");
+            Console.WriteLine("The last digit of " + number + " is 0 and is 0");
         else if (number > 0)
             Console.WriteLine("The last digit of " + number + " is " + number%10 + " and is less than 6 and not 0");
         else
