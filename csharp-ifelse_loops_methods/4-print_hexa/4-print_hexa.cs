fda63b1d1621ccda53721f408afce683a17e909e
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-09 16:49:11 +0200

    Added task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 98; i++)
+            Console.Write("{0} = 0x{1}\n", i, i.ToString("X2"));
+    }
+}
