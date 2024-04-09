96630521b3eb855fc1ab7d509689765cf33c9751
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-09 17:11:57 +0200

    Added task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("{0:00}", i);
+            if (i == 99)
+                Console.Write("\n");
+            else
+                Console.Write(", ");
+        }
+    }
+}
