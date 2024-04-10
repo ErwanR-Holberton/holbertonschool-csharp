bb219b2980e9f3d98462d720c46d322d55be730a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-10 16:52:01 +0200

    added task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string end = ", ";
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (i == 8)
+                    end = "\n";
+                Console.Write("{0}{1}{2}", i, j, end);
+            }
+        }
+    }
+}
