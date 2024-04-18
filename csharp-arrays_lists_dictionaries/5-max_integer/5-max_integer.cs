4cecac8710defbf5e2209004a3571d9395d90b99
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-18 08:45:49 +0200

    Added task 5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int result = myList[0];
+        int i;
+
+        for (i = 1; i < myList.Count; i++)
+        {
+            if (myList[i] > result)
+                result = myList[i];
+        }
+        return result;
+    }
+}
