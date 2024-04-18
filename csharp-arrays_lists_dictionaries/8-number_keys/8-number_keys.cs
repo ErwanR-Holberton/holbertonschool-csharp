0848a10e1a650c2a16b3a5ab0bab56724db5ee11
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-18 09:54:31 +0200

    Added task 8

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int sum = 0;
+        foreach (KeyValuePair<string, string> kvp in myDict)
+            sum += 1;
+
+        return sum;
+    }
+}
