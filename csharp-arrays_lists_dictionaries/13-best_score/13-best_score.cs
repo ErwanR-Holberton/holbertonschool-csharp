2e8d8b91d27fac05b2d93bbd1fbc8d3597539e4e
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-18 10:58:24 +0200

    Added task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        bool first = true;
+        KeyValuePair<string, int> result = new KeyValuePair<string, int>();
+
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (first)
+            {
+                result = kvp;
+                first = false;
+            }
+            else if (kvp.Value > result.Value)
+                result = kvp;
+        }
+        if (first)
+            return "None";
+
+        return result.Key;
+    }
+}
