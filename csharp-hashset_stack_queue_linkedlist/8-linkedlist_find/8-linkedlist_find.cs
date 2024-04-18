35c8545a412d79adb606fb447fb7f47bf4b3cc23
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-18 15:06:26 +0200

    Added task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+
+        foreach (int node_value in myLList)
+            if (value == node_value)
+                return index;
+            else
+                index++;
+
+        return -1;
+    }
+}
