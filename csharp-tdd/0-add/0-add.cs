21d052688f027da8e09d0321898260169fd83217
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-06 17:07:16 +0200

    Add doc for class

--- csharp-tdd/0-add/0-add.cs
+++ csharp-tdd/0-add/0-add.cs
@@ -2,6 +2,9 @@
 
 namespace MyMath
 {
+    /// <summary>
+    /// This is a class that represents a person.
+    /// </summary>
     class Operations
     {
         public static int Add(int a, int b)
