a01ab77bbf9293a868fa0437a2e29af3d9123e0a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-10 22:50:22 +0200

    Added some tests

--- csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -18,5 +18,19 @@ namespace Text.Tests
             int result = Str.CamelCase(s1);
             Assert.AreEqual(2, result);
         }
+        [Test]
+        public void Test3()
+        {
+            string s1 = "";
+            int result = Str.CamelCase(s1);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void Test4()
+        {
+            string s1 = "thisIsATest";
+            int result = Str.CamelCase(s1);
+            Assert.AreEqual(4, result);
+        }
     }
 }
