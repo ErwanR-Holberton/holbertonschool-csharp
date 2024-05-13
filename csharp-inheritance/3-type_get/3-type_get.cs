6f0cff2f35e7d67b884296ef01c8b55c690ffacb
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-13 15:03:17 +0200

    Added methods and show only names

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -5,6 +5,8 @@ class Obj
     public static void Print(object myObj)
     {
         foreach (var attribute in myObj.GetType().GetProperties())
-            Console.WriteLine(attribute);
+            Console.WriteLine(attribute.Name);
+        foreach (var attribute in myObj.GetType().GetMethods())
+            Console.WriteLine(attribute.Name);
     }
 }
