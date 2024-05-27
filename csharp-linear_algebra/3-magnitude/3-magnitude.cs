5a727a143063c156d8093501b1998ecb1478eb9d
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-27 12:54:46 +0200

    Task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.length != 2 && vector.length != 3)
+            return -1;
+
+        int sum = 0;
+        
+        foreach (double value in vector)
+            sum += value * value;
+
+        return Math.Sqrt(value);
+    }
+}
