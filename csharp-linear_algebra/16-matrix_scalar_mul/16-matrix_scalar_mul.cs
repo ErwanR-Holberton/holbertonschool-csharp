d8136e587f94abc973ff70d021732d12b6eeaeea
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-28 15:23:09 +0200

    Task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (!MatrixCheck(matrix))
+            return new double[,] {{-1}};
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i, j] = matrix[i, j] * scalar;
+
+        return result;
+    }
+
+    private static bool MatrixCheck(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        return (rows == 2 && cols == 2) || (rows == 3 && cols == 3);
+    }
+}
