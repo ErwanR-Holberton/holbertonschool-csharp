0dbdd60eec4c852188e4b821f314780d6f66233a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-29 11:45:28 +0200

    Task 26

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        if (!(rows == 2 && cols == 2) || (rows == 3 && cols == 3))
+            return -1;
+
+        if (rows == 2)
+            return matrix[0,0] * matrix[1,1] - matrix[1,0] * matrix[0,1];
+
+        double a, b, c, d, e, f, g, h, i;
+        a = matrix[0,0];
+        b = matrix[0,1];
+        c = matrix[0,2];
+        d = matrix[1,0];
+        e = matrix[1,1];
+        f = matrix[1,2];
+        g = matrix[2,0];
+        h = matrix[2,1];
+        i = matrix[2,2];
+        return a*(e*i - f*h)- b*(d*i- f*g) + c*(d*h- e*g);
+    }
+}
