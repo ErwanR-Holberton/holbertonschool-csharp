46177f003bbc8b096ae2c30d2dcf052520cf2121
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-29 11:24:28 +0200

    Return -1 if wrong direction

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -4,13 +4,13 @@ class MatrixMath
 {
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2))
+        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (direction != 'x' && direction != 'y'))
             return new double[,] {{-1}};
 
         int k = 0;
         double[,] result = new double[2,2];
 
-        if (direction == 'Y')
+        if (direction == 'y')
             k = 1;
 
         for (int i = 0; i < 2; i++)
