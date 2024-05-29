0f8f7f3558655c1a295ee5d3ed4d6234427e8e14
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-29 12:31:24 +0200

    Task 31

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+
+        double det = matrix[0,0] * matrix[1,1] - matrix[1,0] * matrix[0,1];
+
+        if (det == 0)
+            return new double[,] {{-1}};
+
+        return new double[,] {{matrix[1,1] / det, -matrix[0,1] / det}, {-matrix[1,0] / det, matrix[0,0] / det}};
+    }
+}
