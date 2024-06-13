0b43603cfe1ad8df1519fc5587f28baef934d71c
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-13 14:24:24 +0200

    Triple parallel

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -11,27 +11,16 @@ class ImageProcessor
         {
             Bitmap bitmap = new Bitmap(file_name);
 
-            BitmapData lockedimage = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
-            int img_size = lockedimage.Stride * lockedimage.Height;
-            byte[] image_copy = new byte[img_size];
 
-            System.Runtime.InteropServices.Marshal.Copy(lockedimage.Scan0, image_copy, 0, img_size);
 
             Parallel.For(0, bitmap.Height, y =>
             {
-                int yPos = y * lockedimage.Stride;
-                for (int x = 0; x < bitmap.Width; x++)
+                Parallel.For(0, bitmap.Width, x =>
                 {
-                    int pos = yPos + x * 4;
-
-                    image_copy[pos] = (byte)(255 - image_copy[pos]);
-                    image_copy[pos + 1] = (byte)(255 - image_copy[pos + 1]);
-                    image_copy[pos + 2] = (byte)(255 - image_copy[pos + 2]);
-                }
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    bitmap.SetPixel(x, y, Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B));
+                });
             });
-
-            System.Runtime.InteropServices.Marshal.Copy(image_copy, 0, lockedimage.Scan0, img_size);
-            bitmap.UnlockBits(lockedimage);
 
             string[] slip = file_name.Split(new char[] { '/', '.' });
             bitmap.Save(slip[slip.Length - 2] + "_inverse." + slip[slip.Length - 1]);
