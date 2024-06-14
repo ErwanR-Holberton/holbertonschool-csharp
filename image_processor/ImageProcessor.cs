757d83efade7d65e8300c4a753d4a4dd4893aff4
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-14 18:13:38 +0200

    Threads and lockbits

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -15,26 +15,22 @@ class ImageProcessor
             thread.Start();
         });
 
-        TimeSpan limit = new TimeSpan(0, 0, 0, 39, 0);
-        Stopwatch timer = Stopwatch.StartNew();
-        while (limit > timer.Elapsed)
-        {
-
-        }
-
     }
     private static void ProcessImageThread(string file_name)
     {
         Bitmap bitmap = new Bitmap(file_name);
 
-        for (int y = 0; y < bitmap.Height; y++)
-        {
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                Color pixelColor = bitmap.GetPixel(x, y);
-                bitmap.SetPixel(x, y, Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B));
-            }
-        }
+        BitmapData lockedimage = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+        int img_size = lockedimage.Stride * lockedimage.Height;
+        byte[] image_copy = new byte[img_size];
+
+        System.Runtime.InteropServices.Marshal.Copy(lockedimage.Scan0, image_copy, 0, img_size);
+
+        for (int i = 0; i < img_size; i++)
+            image_copy[i] = (byte)(255 - image_copy[i]);
+
+        System.Runtime.InteropServices.Marshal.Copy(image_copy, 0, lockedimage.Scan0, img_size);
+        bitmap.UnlockBits(lockedimage);
 
         string[] slip = file_name.Split(new char[] { '/', '.' });
         bitmap.Save(slip[slip.Length - 2] + "_inverse." + slip[slip.Length - 1]);
