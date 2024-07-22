8dae45fc70675f6ed3d1a64483edc5efb3183ce1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-22 16:34:53 +0200

    More comments

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -6,6 +6,10 @@
 /// <typeparam name="T">The type of elements in the queue.</typeparam>
 public class Queue<T>
 {
+	/// <summary>
+	/// Returns the type of the elements.
+	/// </summary>
+	/// <returns>The type of elements</returns>
 	public Type CheckType()
 	{
 		return typeof(T);
