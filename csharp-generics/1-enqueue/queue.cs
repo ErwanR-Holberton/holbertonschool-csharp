766402b61726882263b2450f6a77fa96ade14128
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-22 18:28:28 +0200

    Task 1

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+public class Queue<T>
+{
+	Node head, tail;
+	int count = 0;
+
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+	public class Node
+	{
+		T value;
+		public Node next = null;
+
+		public Node(T value_input)
+		{
+			value = value_input;
+		}
+	}
+	public void Enqueue(T value)
+	{
+		Node newNode = new Node(value);
+		if (head == null)
+			head = tail = newNode;
+		else
+		{
+			newNode.next = tail;
+			tail = newNode;
+		}
+		count += 1;
+	}
+	public int Count()
+	{
+		return count;
+	}
+
+}
