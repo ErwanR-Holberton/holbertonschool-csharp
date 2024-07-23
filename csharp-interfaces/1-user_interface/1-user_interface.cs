fea0b848d9cd9b5552f3abd2d76ac7d614e9e7f5
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-23 21:21:06 +0200

    Task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,58 @@
+﻿/// <summary> Base class </summary>
+public abstract class Base
+{
+    /// <summary> variable </summary>
+    public string name;
+    /// <summary> Print name and type </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+/// <summary> interface </summary>
+public interface IInteractive
+{
+    /// <summary> interect </summary>
+    void Interact();
+}
+/// <summary> interface </summary>
+public interface IBreakable
+{
+    /// <summary> durability </summary>
+    int durability { get; set; }
+    /// <summary> break </summary>
+    void Break();
+}
+/// <summary> interface </summary>
+public interface ICollectable
+{
+    /// <summary> isCollected </summary>
+    bool isCollected { get; set; }
+    /// <summary> collect </summary>
+    void Collect();
+}
+
+/// <summary> Test class </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+
+    /// <summary> durability </summary>
+    public int durability { get; set; }
+    /// <summary> isCollected </summary>
+    public bool isCollected { get; set; }
+    /// <summary> interect </summary>
+    public void Interact()
+    {
+
+    }
+    /// <summary> break </summary>
+    public void Break()
+    {
+
+    }
+    /// <summary> collect </summary>
+    public void Collect()
+    {
+
+    }
+}
