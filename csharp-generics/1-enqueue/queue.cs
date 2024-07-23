e4995dc3f100e1ed64fe7966bf5d65a0997a5ab9
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-23 09:26:32 +0200

    Add comments

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 
-
+/// <summary>
+/// a generic queue
+/// </summary>
+/// <typeparam name="T">The type of elements in the queue.</typeparam>
 public class Queue<T>
 {
 	Node head, tail;
 	int count = 0;
 
+	/// <summary>
+	/// Returns the type of the elements.
+	/// </summary>
+	/// <returns>The type of elements</returns>
 	public Type CheckType()
 	{
 		return typeof(T);
 	}
+
+	/// <summary>
+	/// Node class
+	/// </summary>
 	public class Node
 	{
 		T value;
+
+		/// <summary>
+		/// next node
+		/// </summary>
 		public Node next = null;
 
+		/// <summary>
+		/// create a node
+		/// </summary>
 		public Node(T value_input)
 		{
 			value = value_input;
 		}
 	}
+
+	/// <summary>
+	/// add node to queue
+	/// </summary>
 	public void Enqueue(T value)
 	{
 		Node newNode = new Node(value);
@@ -27,11 +49,15 @@ public class Queue<T>
 			head = tail = newNode;
 		else
 		{
-			newNode.next = tail;
-			tail = newNode;
+			newNode.next = head;
+			head = newNode;
 		}
 		count += 1;
 	}
+
+	/// <summary>
+	/// return lenght of queue
+	/// </summary>
 	public int Count()
 	{
 		return count;
