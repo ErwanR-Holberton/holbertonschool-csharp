6ba70e3a30472758d21d3de3300213c3465daa4f
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-01 18:16:25 +0200

    Task 2

---
+++ csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace InventoryLibrary
+{
+	public class JSONStorage
+	{
+		private string path = "DB.json";
+		public Dictionary<string, object> objects = new Dictionary<string, object>();
+
+		public Dictionary<string, object> All()
+		{
+			return objects;
+		}
+
+		public void New(object obj)
+		{
+			objects[$"{obj.GetType().Name}.{obj.Id}"] = obj;
+		}
+
+		public void Save()
+		{
+			File.WriteAllText(path, JsonSerializer.Serialize(objects, new JsonSerializerOptions { WriteIndented = true }));
+		}
+
+		public void Load()
+		{
+			if (!File.Exists(path))
+				return; // If the file does not exist, there's nothing to load
+
+			objects = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(File.ReadAllText(path));
+		}
+	}
+}
