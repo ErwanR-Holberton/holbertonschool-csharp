304823a132df053e44cd5fd86af6ad1926836bfa
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-01 17:54:40 +0200

    Task 1

---
+++ csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace InventoryLibrary
+{
+	public class Inventory: BaseClass
+	{
+		public string UserId, ItemId;
+		public int Quantity
+	}
+}
---
+++ csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace InventoryLibrary
+{
+	public class Item: BaseClass
+	{
+		public string name, description;
+		public float price;
+		public List<string> Tags;
+	}
+}
